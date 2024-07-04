7cb0c701fa21f1f51f67f255ce03d7b3559c8121
TrentonYo <trentonyo@gmail.com>
2024-07-04 10:17:28 -0700

    Added interactive input to fill in metadata for cloning templates, updated flow of information from gh

--- startout/github_api.py
+++ startout/github_api.py
@@ -1,35 +1,35 @@
-import platform
+import os.path
+import shlex
 import subprocess
 
 
-def create_repo_from_temp(owner, repo_name, template):
+def create_repo_from_temp(
+    owner: str, repo_name: str, template: str, public: bool = False
+):
     """
     Create a GitHub repo from a provided template.
 
-    :param owner: The owner of the new repo.
-    :param repo_name: The name of the new repo.
-    :param template: The name of the template repo.
+    :param owner: The owner of the new repo to be created, must be a GitHub user/org for which the currently
+    authenticated (`gh auth status`) has permission to create a repo.
+    :param repo_name: The name of the new repo to be created.
+    :param template: The owner and name of the template repo e.g. "StartOut/template-repo"
+    :param public: The visibility for the repo to be created (defaults to private)
     """
-    cmd = [
-        "gh",
-        "repo",
-        "create",
-        f"{owner}/{repo_name}",
-        "--public?",
-        "--clone",
-        f"--template={template}",
-    ]
 
-    if platform.system() == "Windows":
-        cmd = ["cmd", "/c"] + cmd
+    # Example use of shlex (shell lexer) which will help us parse Starterfile
+    cmd_string = f"gh repo create {owner}/{repo_name} --template={template} --clone"
+    cmd = shlex.split(cmd_string)
 
-        print("cmd: ", cmd)
-
-        result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        print(result.stdout)
-        print(result.stderr)
-
+    if public:
+        cmd.append("--public")
     else:
-        result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        print(result.stdout)
-        print(result.stderr)
+        cmd.append("--private")
+
+    result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+    if result.returncode == 0:
+        print(result.stdout.decode())
+        return os.path.join(os.getcwd(), repo_name)
+    else:
+        print(result.stderr.decode())
+        return False
--- startout/main.py
+++ startout/main.py
@@ -1,32 +1,73 @@
 import os
+import sys
 
 import typer
 
 import startout.github_api as gh_r  # temp name
 
+# Initialize the typer CLI
+startout_paths_app = typer.Typer(name="startout-paths")
+
 # Pull Env Variables (Not sure if this is the approach we are wanting)
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN", None)
 TEMPLATE_OWNER = os.getenv("TEMPLATE_OWNER", None)
-EXPRESS_TEMPLATE_NAME = os.getenv("EXPRESS_TEMPLATE_NAME", None)
-NEW_EXPRESS_OWNER = os.getenv(
-    "NEW_EXPRESS_OWNER", None
+PATH_TEMPLATE_NAME = os.getenv("PATH_TEMPLATE_NAME", None)
+NEW_PATH_OWNER = os.getenv(
+    "NEW_PATH_OWNER", None
 )  # TODO: Tightly couple to $GITHUB_TOKEN
-NEW_EXPRESS_NAME = os.getenv("NEW_EXPRESS_NAME", None)
+NEW_PATH_NAME = os.getenv("NEW_PATH_NAME", None)
 
 
 def initialize_path():
-    # initialize_postgresql()
-    initialize_express()
-    # initialize_react()
+    initialized_path = initialize_repo()
+
+    if not initialized_path:
+        print("Failed to initialize new Path, exiting now.", file=sys.stderr)
+    else:
+        pass
+        # initialize_postgresql()
+        # initialize_express()
+        # initialize_react()
 
 
-def initialize_express():
-    print("Initializing Express...")
-    # TODO: Assert that proper env variables are in place
-    gh_r.create_repo_from_temp(
-        NEW_EXPRESS_OWNER, NEW_EXPRESS_NAME, f"{TEMPLATE_OWNER}/{EXPRESS_TEMPLATE_NAME}"
+def initialize_repo():
+    print("Fetching and cloning Path template...")
+
+    # If any environment variables are missing, prompt the user for them interactively
+    _new_path_owner = NEW_PATH_OWNER
+    _new_path_name = NEW_PATH_NAME
+    _template_owner = TEMPLATE_OWNER
+    _template_name = PATH_TEMPLATE_NAME
+
+    # TODO Use some localization scheme for all feedback
+    if _new_path_owner is None:
+        _new_path_owner = input(
+            "Who should be the owner of the new repository?\n"
+            "(Must be a user/org authorized with `gh `auth`): "
+        )
+    if _new_path_name is None:
+        _new_path_name = input(
+            "What should the name of the new repository be?\n"
+            "(Make sure the name isn't already taken): "
+        )
+    if _template_owner is None:
+        _template_owner = input("\nWho is the owner of the Path template?: ")
+
+    if _template_name is None:
+        _template_name = input("\nWhat is the name of the Path template?: ")
+
+    result = gh_r.create_repo_from_temp(
+        _new_path_owner, _new_path_name, f"{_template_owner}/{_template_name}"
     )
     # Update $NEW_PATH_ROOT
+
+    if not result:
+        print("Failed to clone Path template.", file=sys.stderr)
+    else:
+        print(f"Cloned new Path to {result}")
+        os.environ["NEW_PATH_ROOT"] = result
+
+    return result
 
 
 def main(path: str):
