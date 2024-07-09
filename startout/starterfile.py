6e2ca00ff87b40114f87fdf7c679da31fc88eac6
TrentonYo <trentonyo@gmail.com>
2024-07-09 15:14:03 -0700

    Updated starterfile.py with Module classes and schema validation
    Python file 'starterfile.py' has been enhanced with new 'GitModule' and 'CurlModule' classes to specifically handle Git and Curl sourced scripts, respectively. Also, 'Module' class is now capable of validating the instance with a detailed schema including source type and script details. These changes resulted in alterations in the initialization and running process of these classes. The schema validation for Starter class is also updated accordingly.

--- startout/starterfile.py
+++ startout/starterfile.py
@@ -1,24 +1,23 @@
 import shlex
 import subprocess
+import sys
 from typing import TextIO
 import yaml
 
-from schema import Schema, And
-
-
-class Starter:
-    def __init__(self):
-        self.modules = None
+from schema import Schema, And, Or
 
 
 class Module:
     module_schema = Schema(
-        [
-            {
-                "name": And(str, len),
-                "source": And(str, len)
-            }
-        ]
+        {
+            "name": And(str, len),
+            "source": Schema(
+                {
+                    Or("git", "curl", "script", "docker-image", "dockerfile", only_one=True): str
+                }
+            ),
+            "scripts": And(dict, len)
+        }
     )
 
     def __init__(self, name: str, source: str, scripts: dict[str, str]):
@@ -29,21 +28,89 @@ class Module:
         self.source = source
         self.scripts = scripts
 
-    def run(self, script: str) -> subprocess.CompletedProcess[bytes]:
+    def run(self, script: str) -> tuple[str, int]:
         if script not in self.scripts:
             raise ValueError(f"Module \"{self.name}\" does not have script '{script}' in {list(self.scripts.keys())}")
 
         _script = shlex.split(self.scripts[script])
 
-        result = subprocess.run(_script, shell=True)
+        result = subprocess.run(_script, shell=True, check=True, text=True, capture_output=True)
 
-        return result
+        return result.stdout, result.returncode
+
+    def initialize(self):
+        msg, code = self.run("init")
+        print(msg)
+
+        return code == 0
+
+
+class GitModule(Module):
+    def initialize(self):
+        print(f"Going to clone a git repository: {self.source}")
+        return True
+
+
+class CurlModule(Module):
+    def initialize(self):
+        print(f"Going to download and run this script: {self.source}")
+        return True
+
+
+class Starter:
+    starterfile_schema = Schema(
+        {
+            "tools": And(dict, len),
+            "modules": And(dict, len)
+        }
+    )
+
+    def __init__(self, modules: list[Module]):
+        self.modules = modules
+
+    def up(self):
+        if len(self.modules) == 0:
+            print("Nothing to do.")
+            return False
+
+        failed_modules = []
+        for module in self.modules:
+            result = module.initialize()
+
+            if result is False:
+                failed_modules.append(module.name)
+
+        if len(failed_modules) > 0:
+            print("Failed modules:", failed_modules, file=sys.stderr)
+            return False
+
+        return True
 
 
 def parse_starterfile(starterfile_stream: TextIO) -> Starter:
     loaded = yaml.safe_load(starterfile_stream)
-    Module.module_schema.validate(starterfile_stream)
-    return Starter()
+    Starter.starterfile_schema.validate(loaded)
+
+    modules = []
+
+    for module_name in loaded["modules"]:
+        _module = loaded["modules"][module_name]
+        module = Module.module_schema.validate(_module)
+
+        name = module["name"]
+        mode = next(iter(module["source"]))
+        source = module["source"][mode]
+
+        if mode == "git":
+            module = GitModule(name, source, module["scripts"])
+        elif mode == "curl":
+            module = CurlModule(name, source, module["scripts"])
+        else:
+            module = Module(name, source, module["scripts"])
+
+        modules.append(module)
+
+    return Starter(modules)
 
 
 if __name__ == "__main__":
@@ -55,6 +122,8 @@ if __name__ == "__main__":
     # experiment.run("init")
     # experiment.run("ex")
 
+    s = None
     with open("../Starterfile.yaml", "r") as f:
-        parse_starterfile(f)
+        s = parse_starterfile(f)
 
+    s.up()
